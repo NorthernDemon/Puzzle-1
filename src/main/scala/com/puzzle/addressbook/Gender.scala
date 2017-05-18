c3ad21259ada60ba0b1000c5734e7ae36f69ee19
NorthernDemon <ekimov.victor@gmail.com>
2017-05-18 17:59:26 +0300

    Add gender parser from string

--- src/main/scala/com/puzzle/addressbook/Gender.scala
+++ src/main/scala/com/puzzle/addressbook/Gender.scala
@@ -3,3 +3,13 @@ package com.puzzle.addressbook
 sealed trait Gender
 case object Male extends Gender
 case object Female extends Gender
+
+object Gender {
+
+  def apply(s: String): Gender = s match {
+    case "Male" => Male
+    case "Female" => Female
+    case _ => throw new IllegalArgumentException(
+      "Could not parse the gender field. Use [ Male | Female ]")
+  }
+}
