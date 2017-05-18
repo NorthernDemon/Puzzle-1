aa624723b4e92268b8f7e83d7225816d1eb9f37e
NorthernDemon <ekimov.victor@gmail.com>
2017-05-18 20:12:06 +0300

    Add solution for the task 3 with tests (birthdays difference in days)

--- src/main/scala/com/puzzle/addressbook/Main.scala
+++ src/main/scala/com/puzzle/addressbook/Main.scala
@@ -1,6 +1,7 @@
 package com.puzzle.addressbook
 
 import com.github.nscala_time.time.OrderingImplicits._
+import org.joda.time.Interval
 
 object Main {
 
@@ -18,6 +19,15 @@ object Main {
     age match {
       case Oldest => records.minBy(_.map(_.birthDay))
       case Youngest => records.maxBy(_.map(_.birthDay))
+    }
+  }
+
+  def getDaysDifference(firstName1: String, firstName2: String): Option[Long] = {
+    for {
+      first <- records.find(_.get.firstName == firstName1).flatten
+      second <- records.find(_.get.firstName == firstName2).flatten
+    } yield {
+      new Interval(first.birthDay, second.birthDay).toDuration.getStandardDays
     }
   }
 
--- src/test/scala/com/puzzle/addressbook/MainSpec.scala
+++ src/test/scala/com/puzzle/addressbook/MainSpec.scala
@@ -12,13 +12,17 @@ class MainSpec extends FlatSpec with Matchers {
     Main.findByGender(Female) shouldBe 2
   }
 
-  "findByBirthDay(Oldest)" should "print the oldest person on the list" in {
+  "findByBirthDay(Oldest)" should "return the oldest person on the list" in {
     val oldest = AddressRecord.fromString("Wes Jackson, Male, 14/08/74")
     Main.findByAge(Oldest) shouldBe oldest
   }
 
-  "findByBirthDay(Youngest)" should "print the youngest person on the list" in {
+  "findByBirthDay(Youngest)" should "return the youngest person on the list" in {
     val youngest = AddressRecord.fromString("Gemma Lane, Female, 20/11/91")
     Main.findByAge(Youngest) shouldBe youngest
+  }
+
+  "getDaysDifference(firstName1, firstName2)" should "return the number of days between two people's birthdays" in {
+    Main.getDaysDifference("Bill", "Paul") shouldBe Some(2862)
   }
 }
