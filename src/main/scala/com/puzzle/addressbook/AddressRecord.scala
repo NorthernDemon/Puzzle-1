95601baf3cd9d6f8abacc3037741c78739b387c0
NorthernDemon <ekimov.victor@gmail.com>
2017-05-18 18:08:26 +0300

    Add the parser from string to address record

--- src/main/scala/com/puzzle/addressbook/AddressRecord.scala
+++ src/main/scala/com/puzzle/addressbook/AddressRecord.scala
@@ -1,10 +1,34 @@
 package com.puzzle.addressbook
 
 import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
 
 case class AddressBook(records: Seq[AddressRecord])
 
 case class AddressRecord(firstName: String,
-                         secondName: String,
+                         lastName: String,
                          gender: Gender,
                          birthDay: DateTime)
+
+object AddressRecord {
+
+  private val dateTimeFormat = DateTimeFormat.forPattern("dd/MM/YY")
+
+  def fromString(record: String): Option[AddressRecord] = {
+    try {
+      val items = record.replace(",", "").split(" ").map(_.trim).toSeq
+      Some(
+        AddressRecord(
+          firstName = items(0),
+          lastName = items(1),
+          gender = Gender(items(2)),
+          birthDay = dateTimeFormat.parseDateTime(items(3))
+        )
+      )
+    } catch {
+      case error: Throwable =>
+        error.printStackTrace()
+        None
+    }
+  }
+}
