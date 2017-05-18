88fa9423c032b1d205949955c4fb2229f35bd159
NorthernDemon <ekimov.victor@gmail.com>
2017-05-18 17:42:34 +0300

    Changed formatting to two spaces

--- src/main/scala/com/puzzle/addressbook/Resource.scala
+++ src/main/scala/com/puzzle/addressbook/Resource.scala
@@ -3,9 +3,9 @@ package com.puzzle.addressbook
 import scala.io.Source
 
 object Resource {
-    private val currentDirectory = System.getProperty("user.dir")
+  private val currentDirectory = System.getProperty("user.dir")
 
-    def apply(filename: String): Iterator[String] = {
-        Source.fromFile(s"$currentDirectory/src/main/resources/$filename", "UTF-8").getLines()
-    }
+  def apply(filename: String): Iterator[String] = {
+    Source.fromFile(s"$currentDirectory/src/main/resources/$filename", "UTF-8").getLines()
+  }
 }
