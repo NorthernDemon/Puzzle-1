c14f96bd80fe060bac9e98cba254fbde12d1a9ab
NorthernDemon <ekimov.victor@gmail.com>
2017-05-18 18:16:50 +0300

    Changed resource to return a seq

--- src/main/scala/com/puzzle/addressbook/Resource.scala
+++ src/main/scala/com/puzzle/addressbook/Resource.scala
@@ -5,7 +5,7 @@ import scala.io.Source
 object Resource {
   private val currentDirectory = System.getProperty("user.dir")
 
-  def apply(filename: String): Iterator[String] = {
-    Source.fromFile(s"$currentDirectory/src/main/resources/$filename", "UTF-8").getLines()
+  def apply(filename: String): Seq[String] = {
+    Source.fromFile(s"$currentDirectory/src/main/resources/$filename", "UTF-8").getLines().toSeq
   }
 }
