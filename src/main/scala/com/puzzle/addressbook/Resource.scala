580b7b3de25a0d3bc7e0629995f61dc9b37b5e33
NorthernDemon <ekimov.victor@gmail.com>
2017-05-18 17:27:57 +0300

    Add resource reader

---
+++ src/main/scala/com/puzzle/addressbook/Resource.scala
@@ -0,0 +1,11 @@
+package com.puzzle.addressbook
+
+import scala.io.Source
+
+object Resource {
+    private val currentDirectory = System.getProperty("user.dir")
+
+    def apply(filename: String): Iterator[String] = {
+        Source.fromFile(s"$currentDirectory/src/main/resources/$filename", "UTF-8").getLines()
+    }
+}
